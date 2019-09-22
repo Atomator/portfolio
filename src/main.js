d847aef0f4fe9fa9201be7e9d0f335186688d94f
Connor Sparks <ycoloradolego@gmail.com>
2019-09-22 12:44:43 -0600

    Got Sass to Work

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import 'bulma/css/bulma.css'
+import 'src/assets/css/styles.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 
 Vue.config.productionTip = false
