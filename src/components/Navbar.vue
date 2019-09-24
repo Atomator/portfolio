9d319b18470c6e244b30f00c1e00b8db7172469a
Connor Sparks <ycoloradolego@gmail.com>
2019-09-24 06:58:34 -0600

    Created basic navbar; needs styling

---
+++ src/components/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <nav class="level m-xl">
+      <div class="level-right" v-for="(value, key) in pages" v-bind:key="value.id">
+        <div class="level-item">
+          <router-link :to="value"><p class="menuItem has-text-weight-bold has-text-dark subtitle">{{ key }}</p></router-link>
+        </div>
+      </div>
+      <div class="level-left">
+        <div class="level-item">
+          <p>The Verge</p>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar', 
+  data () {
+    return {
+      currentPage: '',
+      pages: {
+        'Home': 'home',
+        'Other': 'other',
+      }
+    }
+  },
+  created: function () {
+    this.currentPage = this.$router.currentRoute.fullPath;
+  },
+}
+</script>
+
+<style>
+
+.menuItem {
+  /* color: #465362; */
+}
+
+</style>
