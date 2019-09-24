1143aa1d38d81a88b5fa32a1ff5e18fc1922c116
Connor Sparks <ycoloradolego@gmail.com>
2019-09-23 22:06:10 -0600

    Added basic homepage and finalized basic colors

--- src/pages/Welcome.vue
+++ src/pages/Welcome.vue
@@ -1,36 +1,32 @@
 <template>
-  <div id="app" class="container">
-    <nav class="level">
-      <div class="level-item has-text-centered">
-        <div>
-          <p class="heading">Tweets</p>
-          <p class="title">3,456</p>
-        </div>
+<!-- Basic app page to sit on the site for now -->
+  <div id="app">
+    <section class="section">
+      <div class="container has-text-centered">
+        <h1 class = "title" id="welcomeMessage">Hello<strong class="has-text-primary">.</strong> </h1>
+        <h2 class="subtitle">
+          While there isn't anything here <strong>now</strong>, there will be soon
+        </h2>
+        <a class="button is-primary" href="https://xd.adobe.com/view/456fbad4-cc7a-4839-6436-7d5ff64d301b-335e/?fullscreen">View the Demo</a>
       </div>
-      <div class="level-item has-text-centered">
-        <div>
-          <p class="heading">Following</p>
-          <p class="title">123</p>
-        </div>
-      </div>
-      <div class="level-item has-text-centered">
-        <div>
-          <p class="heading">Followers</p>
-          <p class="title">456K</p>
-        </div>
-      </div>
-      <div class="level-item has-text-centered">
-        <div>
-          <p class="heading">Likes</p>
-          <p class="title">789</p>
-        </div>
-      </div>
-    </nav>
+    </section>
   </div>
 </template>
 
-<script>
-export default {
+<script>export default {
   name: 'Welcome'
 }
 </script>
+
+<style>
+
+#welcomeMessage {
+  font-weight: 900;
+  font-size: 5em;
+
+  /* Need a better way to manage the text spacing */
+  margin-top: 1em;
+  margin-bottom: 1em;
+}
+
+</style>
