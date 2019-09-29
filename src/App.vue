422f7c606cc98936f6de1d230217e1088375db5f
Connor Sparks <30665220+Atomator@users.noreply.github.com>
2019-09-28 21:26:55 -0600

    Added a footer element

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,42 @@
 
 <template>
   <div id="app">
+    <body class="main">
+      <div class="section">
 
-    <!-- Navbar component -->
-    <navbar></navbar>
+        <!-- Navbar component -->
+        <navbar></navbar>
 
-    <!-- This is the router view that allows for different components (pages) to be showed here -->
-    <router-view/>
+        <!-- This is the router view that allows for different components (pages) to be showed here -->
+        <router-view/>
 
+      </div>
+
+      <!-- Footer component -->
+      <bottom-footer class="footer"></bottom-footer>
+   </body>
   </div>
 </template>
 
 <script scoped>
   import Navbar from './components/Navbar.vue'
+  import BottomFooter from './components/BottomFooter.vue'
 
   export default {
     name: 'App',
-    components: { Navbar },
+    components: { Navbar, BottomFooter }
   }
 </script>
+
+<style scoped>
+.main {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+
+.section {
+  flex: 1;
+  padding: 0;
+}
+</style>
