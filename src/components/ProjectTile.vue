3cbff18c5ab1b6636f428e62a88139adee9b4de9
Connor Sparks <30665220+Atomator@users.noreply.github.com>
2019-10-06 09:42:14 -0600

    Added tags

--- src/components/ProjectTile.vue
+++ src/components/ProjectTile.vue
@@ -3,7 +3,14 @@
     <div class="box has-background-dark">
       <img :src="data.images"/>
       <div id="content">
-        <h1 class="has-text-light title">{{ data.title }}</h1>
+        <h1 class="title-custom has-text-light">{{ data.title }}</h1>
+        <nav class="level">
+          <div class="level-left" v-for="(value) in data.tags" v-bind:key="value.id">
+            <a class="level-item">
+              <span class="tag is-primary is-rounded has-text-weight-bold"> {{ value }} </span>
+            </a>
+          </div>
+        </nav>
       </div>
     </div>
   </div>
@@ -22,13 +29,16 @@ export default {
 
 .box {
   border-radius: 16px;
-  height: 350px;
-  width: 512px;
+  height: 300px;
+  width: 400px;
   padding: 0px;
 }
 
-.title {
+.title-custom {
   font-size: 25px;
+  margin-bottom: 0px;
+  font-weight: 600;
+  line-height: 1.125;
 }
 
 #content {
@@ -38,9 +48,14 @@ export default {
 img {
   border-radius: 16px 16px 0px 0px;
   object-fit: cover;
-  height: 175px;
-  width: 512px;
+  height: 150px;
+  width: 400px;
   padding: 0;
+}
+
+.tag {
+  padding: 0px 12px 0px 12px;
+  font-size: 8px;
 }
 
 </style>
