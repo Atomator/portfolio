1dfd354bd518e12af778eeebb97cc2a11b6cb903
Connor Sparks <ycoloradolego@gmail.com>
2019-10-08 06:08:00 -0600

    Started adding information columns

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -9,6 +9,21 @@
         </p>
       </div>
     </div>
+    <div class="columns is-centered">
+      <div id="interests" class="box column is-one-quarter has-background-dark">
+        <div id="header" class="has-background-primary has-text-centered">
+          <h1 class="box-header has-text-light has-text-weight-bold">Interests</h1>
+        </div>
+        <div id="content" class="has-text-centered">
+          <ul>
+            <li>AWS</li>
+            <li>Machine Learning</li>
+            <li>Game Development</li>
+            <li>Web Development</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -42,6 +57,34 @@ img {
 
 #description {
   margin-top: 24px;
+}
+
+#interests {
+  /* width: 512px; */
+  height: 256px;
+  border-radius: 16px;
+  padding: 0px;
+}
+
+#header {
+  height: 64px;
+  border-radius: 16px 16px 0px 0px;
+  box-shadow: 0px 2px 2px rgba(0, 0, 0, .16);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.box-header {
+  font-size: 25px;
+}
+
+li {
+  color: #F7F7FF;
+  margin: 8px 0px 8px 0px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 </style>
