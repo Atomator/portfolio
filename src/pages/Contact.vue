384319ac3067b032bfb71f9894b7c99c3ec300f5
Connor Sparks <ycoloradolego@gmail.com>
2019-10-10 06:05:55 -0600

    Working on Contact page

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
-    <form name="contact" netlify>
+    <form name="contact" method="post" netlify>
       <p>
-        <label>Name <input type="text" name="name" /></label>
+        <label>Name <input class="input" type="text" name="name" /></label>
       </p>
       <p>
-        <label>Email <input type="email" name="email" /></label>
+        <label>Email <input class="input" type="email" name="email" /></label>
+      </p>
+      <p>
+        <label>Subject <input class="input" type="email" name="email" /></label>
+      </p>
+      <p>
+        <label>Message <textarea class="textarea" placeholder="Textarea"></textarea></label>
       </p>
       <p>
         <button type="submit">Send</button>
