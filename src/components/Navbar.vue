f9c1bb7ab45176daaafc9e1fcf2bf2d089c58a5d
Connor Sparks <ycoloradolego@gmail.com>
2019-10-10 05:46:14 -0600

    Added contact page

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -40,6 +40,7 @@ export default {
         'Home': '/',
         'Projects': '/projects',
         'About': '/about',
+        'Contact': '/contact',
       }
     }
   },
---
+++ src/pages/Contact.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <form name="contact" netlify>
+      <p>
+        <label>Name <input type="text" name="name" /></label>
+      </p>
+      <p>
+        <label>Email <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Contact',
+}
+</script>
+
+<style scoped>
+</style>
