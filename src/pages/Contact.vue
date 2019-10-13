2ad63035417ac901d4eda208949ef2a6fe7ac680
Connor Sparks <ycoloradolego@gmail.com>
2019-10-13 07:26:58 -0600

    Style form; testing if it works

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -8,19 +8,23 @@
       netlify-honeypot="bot-field"
     >
       <input type="hidden" name="form-name" value="contact" />
-      <div>  
-        <label for="name">Name:</label> 
-        <input type="text" name="name" required/>
+      <div class="columns">
+        <div class="column is-half">  
+          <label for="name" class="label">Name:</label> 
+          <input type="text" name="name" class="input" required/>
+        </div>
+        <div class="column is-half">
+          <label for="email" class="label">Email:</label>
+          <input type="email" name="email" class="input" required/>
+        </div>
       </div>
       <div>
-        <input type="email" name="email" required/>
-        <label for="email">Email:</label>
+        <label for="message" class="label">Message:</label>
+        <textarea name="message" class="textarea" required></textarea>
       </div>
-      <div>
-        <textarea name="message" required></textarea>
-        <label for="message">Message:</label>
+      <div class="field is-grouped is-grouped-centered">
+        <button type="submit" value="Send message" class="button is-primary">Send</button>
       </div>
-      <button type="submit" value="Send message">Send</button>
     </form>
   </div>
 </template>
@@ -30,3 +34,9 @@ export default {
   name: 'Contact',
 }
 </script>
+
+<style scoped>
+.field {
+  margin-top: 15px;
+}
+</style>
