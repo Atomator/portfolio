4f5218a34dca7563fad6a8fa0bf51efc65af031b
Connor Sparks <30665220+Atomator@users.noreply.github.com>
2019-10-13 10:07:49 -0600

    Finished contact from

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <div class="container has-text-centered">
+      <h1 class="title">Ask Away<strong class="has-text-primary">.</strong></h1>
+    </div>
     <form 
       name="contact" 
       action="/#/" 
@@ -10,20 +13,20 @@
       <input type="hidden" name="form-name" value="contact" />
       <div class="columns">
         <div class="column is-half">  
-          <label for="name" class="label">Name:</label> 
+          <label for="name" class="label">Name</label> 
           <input type="text" name="name" class="input" required/>
         </div>
         <div class="column is-half">
-          <label for="email" class="label">Email:</label>
+          <label for="email" class="label">Email</label>
           <input type="email" name="email" class="input" required/>
         </div>
       </div>
       <div>
-        <label for="message" class="label">Message:</label>
+        <label for="message" class="label">Message</label>
         <textarea name="message" class="textarea" required></textarea>
       </div>
       <div class="field is-grouped is-grouped-centered">
-        <button type="submit" value="Send message" class="button is-primary">Send</button>
+        <button type="submit" value="Send message" class="button is-primary has-text-weight-bold">Submit</button>
       </div>
     </form>
   </div>
@@ -36,7 +39,22 @@ export default {
 </script>
 
 <style scoped>
-.field {
-  margin-top: 15px;
+.button {
+  height: 50px;
+  width: 175px;
+  border-radius: 10px;
+  box-shadow: 0 6px 6px rgba(10, 10, 10, 0.15);
+  font-size: 18px;
+  margin-top: 100px;
+}
+
+.title {
+  font-size: 35px;
+  margin-bottom: 100px;
+  font-weight: 900;
+}
+
+form {
+  width: 40vw;
 }
 </style>
