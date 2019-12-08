81c201cee0ef157e9640b57a4f7323803da32412
Connor Sparks <ycoloradolego@gmail.com>
2019-12-07 21:06:27 -0700

    Started Qlearning page

---
+++ src/pages/projects/Qlearning.vue
@@ -0,0 +1,42 @@
+<template>
+<!-- Basic app page to sit on the site for now -->
+  <div id="app">
+    <div class="container">
+      <h1 class="primary-title">Qlearning<strong class="has-text-primary">.</strong> </h1>
+      <h2 class="subtitle has-text-weight-medium">
+        Teaching My Computer to Beat Me at Snake
+      </h2>
+      <a class="button-primary is-primary is-large has-shadow" href="https://github.com/Atomator/VRKit">
+        <a class="icon">
+          <img src="../../assets/media icons/github.svg" class="logo">
+        </a>
+        <!-- <p class="has-text-weight-bold">Explore</p> -->
+      </a>
+      <a class="button is-primary is-large has-shadow" href="https://medium.com/@coloradolego/how-to-use-arkit-and-unity-to-create-6dof-mobile-vr-f03de54e2edd">
+        <a class="icon">
+          <img src="../../assets/media icons/medium.svg" class="logo">
+        </a>
+        <!-- <p class="has-text-weight-bold">Explore</p> -->
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Qlearning'
+}
+</script>
+
+<style scoped>
+@import '../../assets/css/projects.css';
+
+.buttonc {
+    padding: 30px 35px 30px 35px;
+    margin-right: 36px;
+    margin-bottom: 35px;
+    border-radius: 10px;
+    box-shadow: 0 6px 6px rgba(10, 10, 10, 0.15);
+}
+
+</style>
