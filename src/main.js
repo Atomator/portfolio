8a7de8a355d29e54261057eece1178c0fa5d195c
Atomator <30665220+Atomator@users.noreply.github.com>
2020-08-12 22:14:44 -0600

    Added tracking

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import router from './router'
 
 import 'src/assets/css/styles.css'
 import '@fortawesome/fontawesome-free/css/all.css'
+import VueGtag from 'vue-gtag'
+
+
+Vue.use(VueGtag, {
+  config: { id: "UA-175337381-1" }
+}, router)
 
 Vue.config.productionTip = false
 
